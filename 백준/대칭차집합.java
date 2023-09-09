afd80de14a1314a1489ec9ff1689fefb5a559aeb
HongYeseul <ghd8119@naver.com>
2023-09-09 23:19:35 +0900

    230909 백준#1269 대칭차집합 array->set 교체했는데 동일하게 틀린다. 왜 그런지 좀 더 고민 해봐야겠음.

--- 백준/대칭차집합.java
+++ 백준/대칭차집합.java
@@ -1,9 +1,12 @@
 package 백준;
+// #1269 대칭 차집합 (https://www.acmicpc.net/problem/1269)
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.StringTokenizer;
 
 public class 대칭차집합 {
@@ -26,16 +29,24 @@ public class 대칭차집합 {
         for(int i=0; i<bNum; i++){
             b[i] = Integer.parseInt(st.nextToken());
         }
-        Arrays.sort(a);
-        Arrays.sort(b);
+
+        Set<Integer> setA = new HashSet<Integer>(Arrays.asList(a));
+        Integer[] arrA = setA.toArray(new Integer[setA.size()]);
+
+        Set<Integer> setB = new HashSet<Integer>(Arrays.asList(b));
+        Integer[] arrB = setB.toArray(new Integer[setB.size()]);
+
+
+        Arrays.sort(arrA);
+        Arrays.sort(arrB);
 
         int result = 0;
-        for(int i=0; i<aNum; i++){
-            if(Arrays.binarySearch(b, a[i])>=0)
+        for(int i=0; i<arrA.length; i++){
+            if(Arrays.binarySearch(arrB, arrA[i])>=0)
                 result++;
         }
-        for(int i=0; i<bNum; i++){
-            if(Arrays.binarySearch(a, b[i])>=0)
+        for(int i=0; i<arrB.length; i++){
+            if(Arrays.binarySearch(arrA, arrB[i])>=0)
                 result++;
         }
 
