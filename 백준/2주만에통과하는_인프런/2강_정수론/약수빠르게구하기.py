e6041d36cbc92e9f3028de14099935822eedca05
HongYeseul <ghd8119@naver.com>
2023-09-21 22:05:29 +0900

    ✅⭐백준#14232, #11653 소인수분해

--- 백준/2주만에통과하는_인프런/2강_정수론/약수빠르게구하기.py
+++ 백준/2주만에통과하는_인프런/2강_정수론/약수빠르게구하기.py
@@ -1,34 +1,17 @@
-# n = int(input())
-# result = set()
+# 백준#14232 보석도둑(https://www.acmicpc.net/problem/14232)
+# 11653(소인수분해, https://www.acmicpc.net/problem/11653) 문제와 동일한 문제!
 
-# result.add(1)
-# for i in range(2, int(n**0.5)+1):
-#     if(n%i == 0):
-#         result.add(i)
-#         result.add(int(n/i))
-
-# print(*result)
-
-# for i in range(int(n**0.5)+1, 0, -1):
-#     if(n%i == 0):
-#         if(i == 1):
-#             print(1)
-#             print(int(n/i))
-#             break
-#         else:
-#             print(2)
-#             print(i, int(n/i))        
-#             break
 n = int(input())
-arr = list(map(int, input().split()))
-cnt=0
 
-for j in arr:
-    temp = 0
-    for i in range(1, int(j**0.5)+1):
-        if(j%i == 0 and j!=1):
-            temp+=1
-    if(temp == 1): cnt+=1
+index = 2
+temp = n
+result = list()
+while( index <= (temp**(0.5) +1)):
+    while(n % index == 0):
+        result.append(index)
+        n //= index
+    index+=1
 
-print(cnt)
-# print(*result)
+if(n!=1): result.append(n)
+print(len(result))
+print(*result)
