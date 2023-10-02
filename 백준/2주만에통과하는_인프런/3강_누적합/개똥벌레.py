9ee0e20d572e82059dde343ffc41a4e395c771d4
HongYeseul <ghd8119@naver.com>
2023-10-02 10:01:50 +0900

    ✅ 백준#3020 개똥벌레: 파이썬 시간초과 답은 stdin ...

--- 백준/2주만에통과하는_인프런/3강_누적합/개똥벌레.py
+++ 백준/2주만에통과하는_인프런/3강_누적합/개똥벌레.py
@@ -1,15 +1,16 @@
 # 백준#3020 개똥벌레(https://www.acmicpc.net/problem/3020)
+from sys import stdin
 
-N, H = map(int, input().split())
+N, H = map(int, stdin.readline().split())
 arr = list()
 
 for i in range(N):
-    arr.append(int(input()))
+    arr.append(int(stdin.readline()))
 
 
 prefix = [ 0 for _ in range(H+1)]
+sum = [0 for _ in range(H+1)]
 for i in range(N):
-    
     if(i%2 == 0): # 짝수
         prefix[0] += 1
         prefix[arr[i]] += -1
@@ -18,13 +19,10 @@ for i in range(N):
     
     # print(prefix)
 
-sum = [0 for _ in range(H+1)]
 mini = prefix[0]
 for i in range(1, H+1):
     sum[i] += sum[i-1]+prefix[i-1]
-    # print("비교: ", sum[i], mini)
     if(sum[i] <= mini):
-        # print("smallest: ",sum[i])
         mini = sum[i]
 
 cnt = 0
