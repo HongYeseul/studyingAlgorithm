b86a48f7113e29e2f54814f3283cf6dc3aef4ad1
Yeseul <50395809+HongYeseul@users.noreply.github.com>
2024-04-10 08:46:03 +0900

    [Silver III] Title: N과 M (6), Time: 124 ms, Memory: 109240 KB -BaekjoonHub

---
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.py
@@ -0,0 +1,22 @@
+N, M = map(int, input().split())
+arr = list(map(int, input().split()))
+arr.sort()
+
+result = []
+
+def recurse(n):
+    if n == M:
+        print(*result)
+        return
+    
+    for i in range(0, N):
+        if arr[i] in result:
+            continue
+        if len(result) > 0 and max(result) > arr[i]:
+            continue
+
+        result.append(arr[i])
+        recurse(n+1)
+        result.pop()
+
+recurse(0)
