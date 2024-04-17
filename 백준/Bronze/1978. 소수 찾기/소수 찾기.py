928abb3a38674a64787318067abc4fe2d01e5ca3
Yeseul <50395809+HongYeseul@users.noreply.github.com>
2024-04-17 17:57:22 +0900

    [Bronze II] Title: 소수 찾기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/1978. 소수 찾기/소수 찾기.py
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.py
@@ -4,12 +4,10 @@ result = 0
 
 for x in arr:
     flag = True
-    if x == 1:
-        flag = False
     for i in range(2, int(x**0.5)+1):
         if x % i == 0:
             flag = False
-    if flag == True:
+    if x != 1 and flag == True:
         result+=1
 
 print(result)
