ab33f3ccb2f131579ffaf41a522ef2f877549dc9
Yeseul <50395809+HongYeseul@users.noreply.github.com>
2024-04-20 13:21:22 +0900

    [Silver II] Title: 도영이가 만든 맛있는 음식, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/2961. 도영이가 만든 맛있는 음식/도영이가 만든 맛있는 음식.py
@@ -0,0 +1,21 @@
+N = int(input())
+ingre = [list(map(int, input().split())) for _ in range(N)]
+answer = 99999999
+
+def recurse(index, s, b, use):
+    global answer
+    
+    if (index == N): # 모든 재료 사용
+        if (use == 0): # 아무것도 사용 안했을 때 
+            return
+        answer = min(answer, abs(s - b))
+        return
+
+    # 재료를 사용했을 때
+    recurse(index+1, s*ingre[index][0], b+ingre[index][1], use+1)
+
+    # 재료를 사용하지 않았을 때
+    recurse(index+1, s, b, use)
+
+recurse(0, 1, 0, 0)
+print(answer)
