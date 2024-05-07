5e015ff819d0a6b82b19cc460fb315094677caf8
Yeseul <50395809+HongYeseul@users.noreply.github.com>
2024-05-07 16:20:32 +0900

    Update 전깃줄.py

--- 백준/Gold/2565. 전깃줄/전깃줄.py
+++ 백준/Gold/2565. 전깃줄/전깃줄.py
@@ -1,3 +1,6 @@
+# 핵심
+# 교차된 줄이 없는 그룹이 최대 였을 때 = 오름차순인 그룹의 최대
+
 import sys
 N = int(sys.stdin.readline())
 arr = [list(map(int, sys.stdin.readline().split())) for _ in range(N)]
@@ -15,4 +18,5 @@ for i in range(N):
             temp = max(temp, dp[j])
     dp[i] = temp+1
 
+# N - LIS(전체 전깃줄 - 교차 없는 전깃줄 중 최대 길이)
 print(N - max(dp))
