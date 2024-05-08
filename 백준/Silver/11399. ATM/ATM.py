c2d1a8b14f307632efe3444a4daf91828fb466b3
Yeseul <50395809+HongYeseul@users.noreply.github.com>
2024-05-08 10:06:03 +0900

    [Silver IV] Title: ATM, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.py
+++ 백준/Silver/11399. ATM/ATM.py
@@ -1,13 +1,10 @@
 import sys
 
 N = int(sys.stdin.readline())
-arr = list(map(int, sys.stdin.readline().split()))
-dp = [0 for _ in range(N)]
+arr = sorted(list(map(int, sys.stdin.readline().split())))
 
-arr.sort()
+result = 0
+for i in range(N):
+    result += ((N - i) * arr[i])
 
-dp[0] = arr[0]
-for i in range(1, N):
-    dp[i] = dp[i-1] + arr[i]
-
-print(sum(dp))
+print(result)
