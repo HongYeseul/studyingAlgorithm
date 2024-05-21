29386ff626064cf056ea7e3dca6929bbd8a6823f
Yeseul <50395809+HongYeseul@users.noreply.github.com>
2024-05-21 22:10:48 +0900

    [Silver II] Title: 랜선 자르기, Time: 64 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.py
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.py
@@ -1,19 +1,27 @@
 import sys
+# 이분 탐색을 사용하는 이유
+# 1 2 3 4 5 6 7 8 숫자 중에 5를 검색할 때
+# 순차 탐색이 아니라 4 > 6 > 5 순으로 탐색의 범위를 반으로 줄이려고 사용한다.
 
-K, N = map(int, input().split())
+K, N = map(int, sys.stdin.readline().split())
+
 lan = [int(sys.stdin.readline()) for _ in range(K)]
-start, end = 1, max(lan)
 
-while start<=end:
-    mid = (end + start) // 2
-    lines = 0
+def solve():
+    start, end = 1, max(lan)
 
-    for i in lan:
-        lines += (i // mid)
-    
-    if lines >= N:
-        start = mid + 1
-    else:
-        end = mid - 1
+    while start <= end:
+        mid = (start + end) // 2
+        lines = 0
 
-print(end)
+        for x in lan:
+            lines += (x // mid)
+        
+        if lines < N:
+            end = mid -1
+        else:
+            start = mid +1
+    return end
+
+
+print(solve())
