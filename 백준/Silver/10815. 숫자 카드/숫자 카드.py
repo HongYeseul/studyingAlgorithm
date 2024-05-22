cac8550664e15d6eafe02ce1fcaf858c6f067fd6
Yeseul <50395809+HongYeseul@users.noreply.github.com>
2024-05-22 10:28:04 +0900

    [Silver V] Title: 숫자 카드, Time: 732 ms, Memory: 123572 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.py
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.py
@@ -5,22 +5,32 @@ arr = list(map(int, sys.stdin.readline().split()))
 M = int(sys.stdin.readline())
 toFind = list(map(int, sys.stdin.readline().split()))
 
-arr.sort()
+# arr.sort()
 
-def search(target):
-    start = 0
-    end = N-1
+# def search(target):
+#     start = 0
+#     end = N-1
 
-    while start <= end:
-        mid = (start + end) // 2
+#     while start <= end:
+#         mid = (start + end) // 2
 
-        if arr[mid] == target:
-            return 1
-        elif arr[mid] < target:
-            start = mid +1
-        else:
-            end = mid -1
-    return 0
+#         if arr[mid] == target:
+#             return 1
+#         elif arr[mid] < target:
+#             start = mid +1
+#         else:
+#             end = mid -1
+#     return 0
 
-for target in toFind:
-    print(search(target), end=" ")
+# for target in toFind:
+#     print(search(target), end=" ")
+
+n_dict = {}
+for i in range(N):
+    n_dict[arr[i]] = i
+
+for x in toFind:
+    if x in n_dict:
+        print("1", end=" ")
+    else:
+        print("0", end=" ")
