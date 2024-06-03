a56217aae01774e64f2a99167e676bfee5a8d637
Yeseul <50395809+HongYeseul@users.noreply.github.com>
2024-06-03 21:06:51 +0900

    [Gold IV] Title: 최단경로, Time: 904 ms, Memory: 72488 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.py
+++ 백준/Gold/1753. 최단경로/최단경로.py
@@ -1,37 +1,40 @@
 import sys
-from collections import deque
 import heapq
 
+# 입력처리
 V, E = map(int, sys.stdin.readline().split())
-
 start = int(sys.stdin.readline())
 
+# 그래프 초기화
 links = [[] for _ in range(V+1)]
-INF = 1e9 # 1,000,000,000
+INF = 1e9 # 무한대
 dist = [INF for _ in range(V+1)]
 
+# 간선 정보 입력
 for _ in range(E):
     A,B,C = map(int, sys.stdin.readline().split())
     links[A].append([B, C])
 
-# BFS !
 
+# 우선순위 큐를 이용한 다익스트라 알고리즘
 q = []
 heapq.heappush(q, [0, start])
 dist[start] = 0
 
-while q: # 배열에 아무것도 없으면 false가 된다
-    # 우선 순위 큐를 이용해서, 거리를 보고 정렬(dist를 보고 순서를 바꾸는 코드!)
-    _w, node = heapq.heappop(q)
+while q: 
+    current_distance, node = heapq.heappop(q)
+
+    # 현재 노드까지의 최단거리가 이미 구해진 거리보다 크다면 무시
+    if current_distance > dist[node]:
+        continue
 
     for nxt, weight in links[node]:
-        # 1. 각각이 노드에 값을 업데이트
-        # 2. 만약에 여러 경로가 있다면 min 비교
-        # 3. 시작점으로부터 거리를 확인 후 짧은 순서대로 탐색
+        # 현재 노드를 거쳐 다른 노드로 이동하는 거리가 더 짧을 경우 업데이트
         if dist[node] + weight < dist[nxt]:
             dist[nxt] = dist[node] + weight
             heapq.heappush(q, [dist[nxt], nxt])
 
+# 결과 출력
 for j in range(1,V+1):
     if dist[j] == INF:
         print("INF")
